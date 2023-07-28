aac95979ec31236c19aa127433dc06fc50552fee
Martin <tincho16.06@gmail.com>
2023-07-28 00:01:50 -0300

    des1

---
+++ src/components/CartWindget.jsx
@@ -0,0 +1,9 @@
+import { BsFillCartFill } from "react-icons/bs";
+
+const CartWindget = (cont) =>{
+    return (
+        <div><BsFillCartFill>{cont}</BsFillCartFill></div>
+    )    
+}
+
+export default CartWindget
